cd9755f83ba85507a4fcd7424d04dc74e427f1c0
kenware <ejykken@gmail.com>
2017-11-02 22:21:46 +0100

    t

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.json({ type: 'application/json'}));
 let route = require('./dist/route/route.js');
 
 app.use('/api', route);
-app.listen('5000', function(){
+app.listen(process.env.PORT || '5000', function(){
 	console.log('server is running');
 });
 
