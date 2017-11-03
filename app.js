276cdd36ff63caaedb27a363d74de8aab6451f9e
kenware <ejykken@gmail.com>
2017-11-03 12:15:09 +0100

    deploy

--- app.js
+++ app.js
@@ -8,7 +8,22 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json({ type: 'application/json'}));
 import route from './server/route/index.js';
+const { Client } = require('pg');
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+  if (err) throw err;
+  for (let row of res.rows) {
+    console.log(JSON.stringify(row));
+  }
+  client.end();
+});
 app.use('/api', route);
 
 
@@ -19,5 +34,4 @@ app.get('*', (req, res) => res.status(200).send({
 app.listen(process.env.PORT || '5000', () => {
 	console.log('server is running');
 });
-
 export default app;
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
-[![Coverage Status](https://coveralls.io/repos/github/kenware/more-recipes/badge.svg)](https://coveralls.io/github/kenware/more-recipes)
+{<img src="https://coveralls.io/repos/github/kenware/more-recipes/badge.svg?branch=tester" alt="Coverage Status" />}[https://coveralls.io/github/kenware/more-recipes?branch=tester]
+
 
 [![Build Status](https://travis-ci.org/kenware/more-recipes.svg?branch=tester)](https://travis-ci.org/kenware/more-recipes)
 =======
